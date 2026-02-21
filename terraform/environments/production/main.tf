0f87ad6dd7303dd059e576fce074a5f1cebbacc5
Daniel Osmond <dosmond37@gmail.com>
2026-02-21 16:45:10 -0700

    Adjustments to veriables and outputs

--- terraform/environments/production/main.tf
+++ terraform/environments/production/main.tf
@@ -106,9 +106,10 @@ resource "null_resource" "control_plane_build" {
 module "control_plane_worker" {
   source = "../../modules/cloudflare-worker"
 
-  account_id  = var.cloudflare_account_id
-  worker_name = "open-inspect-control-plane-${local.name_suffix}"
-  script_path = local.control_plane_script_path
+  account_id            = var.cloudflare_account_id
+  worker_name           = "open-inspect-control-plane-${local.name_suffix}"
+  workers_dev_subdomain = var.cloudflare_worker_subdomain
+  script_path           = local.control_plane_script_path
 
   kv_namespaces = [
     {
@@ -193,9 +194,10 @@ resource "null_resource" "slack_bot_build" {
 module "slack_bot_worker" {
   source = "../../modules/cloudflare-worker"
 
-  account_id  = var.cloudflare_account_id
-  worker_name = "open-inspect-slack-bot-${local.name_suffix}"
-  script_path = local.slack_bot_script_path
+  account_id            = var.cloudflare_account_id
+  worker_name           = "open-inspect-slack-bot-${local.name_suffix}"
+  workers_dev_subdomain = var.cloudflare_worker_subdomain
+  script_path           = local.slack_bot_script_path
 
   kv_namespaces = [
     {
@@ -252,9 +254,10 @@ module "github_bot_worker" {
   count  = var.enable_github_bot ? 1 : 0
   source = "../../modules/cloudflare-worker"
 
-  account_id  = var.cloudflare_account_id
-  worker_name = "open-inspect-github-bot-${local.name_suffix}"
-  script_path = local.github_bot_script_path
+  account_id            = var.cloudflare_account_id
+  worker_name           = "open-inspect-github-bot-${local.name_suffix}"
+  workers_dev_subdomain = var.cloudflare_worker_subdomain
+  script_path           = local.github_bot_script_path
 
   service_bindings = [
     {
@@ -307,9 +310,10 @@ module "linear_bot_worker" {
   count  = var.enable_linear_bot ? 1 : 0
   source = "../../modules/cloudflare-worker"
 
-  account_id  = var.cloudflare_account_id
-  worker_name = "open-inspect-linear-bot-${local.name_suffix}"
-  script_path = local.linear_bot_script_path
+  account_id            = var.cloudflare_account_id
+  worker_name           = "open-inspect-linear-bot-${local.name_suffix}"
+  workers_dev_subdomain = var.cloudflare_worker_subdomain
+  script_path           = local.linear_bot_script_path
 
   kv_namespaces = [
     {
