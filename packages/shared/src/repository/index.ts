b60bedfe494e65de7e9326d3cfc992216ef94101
Cole Murray <colemurray.cs@gmail.com>
2026-07-29 21:50:11 -0700

    refactor(shared): curate domain interface exports

--- packages/shared/src/repository/index.ts
+++ packages/shared/src/repository/index.ts
@@ -1,4 +1,46 @@
-export * from "../git";
-export * from "../types/repositories";
-export * from "../types/repository-catalog";
+export {
+  BRANCH_PREFIX,
+  extractSessionIdFromBranch,
+  generateBranchName,
+  isInspectBranch,
+  normalizeBranchName,
+} from "../git";
+export {
+  decodeRepositoryPathSegments,
+  encodeRepositoryPathSegments,
+  formatRepositoryFullName,
+  MAX_SESSION_REPOSITORIES,
+  MAX_TARGET_REPOSITORIES,
+  normalizeOptionalRepositoryPair,
+  parseRepositoryFullName,
+  prArtifactBelongsToRepo,
+  repositoriesInputSchema,
+  repositoryInputSchema,
+  RepositoryPairValidationError,
+  sessionRepositoriesInputSchema,
+  sessionRepositoryStateSchema,
+} from "../types/repositories";
+export type {
+  RepositoryInput,
+  RepositoryPair,
+  RepositoryRef,
+  SessionListRepository,
+  SessionRepositoryState,
+} from "../types/repositories";
+export {
+  controlPlaneReposResponseSchema,
+  enrichedRepositorySchema,
+  installationRepositorySchema,
+  repoConfigSchema,
+  repoMetadataSchema,
+} from "../types/repository-catalog";
+export type {
+  ClassificationResult,
+  ControlPlaneRepo,
+  ControlPlaneReposResponse,
+  EnrichedRepository,
+  InstallationRepository,
+  RepoConfig,
+  RepoMetadata,
+} from "../types/repository-catalog";
 export type { ConfidenceLevel } from "../types/statuses";
