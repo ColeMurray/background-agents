932f2080dc633948ab2d64c866f0f39aa60be96a
Cole Murray <colemurray.cs@gmail.com>
2026-09-06 20:51:45 -0700

    Install Amazon Linux VNC libraries in the system loader path

--- packages/sandbox-images/install/os/amazon-linux.sh
+++ packages/sandbox-images/install/os/amazon-linux.sh
@@ -20,13 +20,15 @@ tar -xJf "$build_dir/fluxbox.tar.xz" -C "$build_dir"
 (cd "$build_dir/fluxbox-$FLUXBOX_VERSION" && sed -i 's/text_prop.value > 0/text_prop.value != 0/' util/fluxbox-remote.cc && ./configure --disable-imlib2 && make -j2 && make install)
 download_checked "https://github.com/LibVNC/libvncserver/archive/refs/tags/LibVNCServer-$LIBVNCSERVER_VERSION.tar.gz" "$LIBVNCSERVER_SHA256" "$build_dir/libvncserver.tar.gz"
 tar -xzf "$build_dir/libvncserver.tar.gz" -C "$build_dir"
-cmake -S "$build_dir/libvncserver-LibVNCServer-$LIBVNCSERVER_VERSION" -B "$build_dir/libvncserver-build" -DWITH_GCRYPT=OFF -DWITH_GNUTLS=OFF -DWITH_FFMPEG=OFF -DWITH_PNG=OFF -DWITH_SDL=OFF -DWITH_SYSTEMD=OFF
+# Use the system library directory: Amazon Linux does not load /usr/local/lib64 by default.
+cmake -S "$build_dir/libvncserver-LibVNCServer-$LIBVNCSERVER_VERSION" -B "$build_dir/libvncserver-build" -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib64 -DWITH_GCRYPT=OFF -DWITH_GNUTLS=OFF -DWITH_FFMPEG=OFF -DWITH_PNG=OFF -DWITH_SDL=OFF -DWITH_SYSTEMD=OFF
 cmake --build "$build_dir/libvncserver-build" --parallel 2
 cmake --install "$build_dir/libvncserver-build"
 ldconfig
 download_checked "https://github.com/LibVNC/x11vnc/archive/refs/tags/$X11VNC_VERSION.tar.gz" "$X11VNC_SHA256" "$build_dir/x11vnc.tar.gz"
 tar -xzf "$build_dir/x11vnc.tar.gz" -C "$build_dir"
 (cd "$build_dir/x11vnc-$X11VNC_VERSION" && CFLAGS=-fcommon PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig:/usr/local/lib/pkgconfig ./autogen.sh && make -j2 && make install)
+x11vnc -version
 mkdir -p /usr/share/novnc
 download_checked "https://github.com/novnc/noVNC/archive/refs/tags/v$NOVNC_VERSION.tar.gz" "$NOVNC_SHA256" "$build_dir/novnc.tar.gz"
 tar -xzf "$build_dir/novnc.tar.gz" -C /usr/share/novnc --strip-components=1
